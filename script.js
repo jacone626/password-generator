6159f6f349be5e6a515f1cb41d68e2a485895bc7
James A Cone <jacone626@gmail.com>
2023-02-08 15:06:09 -0500

    added a function that has a prompt show up when you click on the generate password button

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//If you click on the button, then prompt for password length, include lowercase, include uppercase, numbers, and special characters.
+//once input is validated, password is generated on page or in alert
+
+document.querySelector("generate").addEventListener("click", writePassword);
+
+function writePassword() {
+  var passwordPrompt = prompt("Please provide a password");
+  alert(passwordPrompt)
+}
